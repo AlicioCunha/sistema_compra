611fd158096d5b2fe89e92a8e86d04096545ad86
alicio <alicio.fcn@gmail.com>
2020-10-31 18:47:55 -0300

    Metodos de inserir na tabela de mercado

--- src/main/java/br/com/sistemacompra/model/dao/MercadoDAO.java
+++ src/main/java/br/com/sistemacompra/model/dao/MercadoDAO.java
@@ -1,2 +1,40 @@
-package br.com.sistemacompra.model.dao;public class MercadoDAO {
+package br.com.sistemacompra.model.dao;
+
+import br.com.sistemacompra.data.ConexaoJDBC;
+import br.com.sistemacompra.data.ConexaoMysqlJDBC;
+import br.com.sistemacompra.model.Mercado;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MercadoDAO {
+
+    private final ConexaoJDBC conexao;
+    private PreparedStatement stmt;
+    private ResultSet rs;
+    private String sqlQuery="";
+
+    public MercadoDAO() throws SQLException, ClassNotFoundException {
+        this.conexao = new ConexaoMysqlJDBC();
+    }
+
+    public void inserir(String mercado) throws SQLException, ClassNotFoundException {
+
+        String sqlQuery = "insert into mercado(descricao) values(?);";
+
+        try {
+            PreparedStatement stmt = this.conexao.getConnection().prepareStatement(sqlQuery);
+            stmt.setString(1, mercado);
+
+            stmt.execute();
+
+            this.conexao.commit();
+        } catch (SQLException e) {
+            this.conexao.rollback();
+            throw e;
+        }
+    }
+
+
 }
