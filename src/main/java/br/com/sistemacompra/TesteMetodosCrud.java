cd5fef1cb3f535986c9fae177adb2a66948871b3
alicio <alicio.fcn@gmail.com>
2020-10-31 18:48:17 -0300

    Classe de teste de insercao no mercado

--- src/main/java/br/com/sistemacompra/TesteMetodosCrud.java
+++ src/main/java/br/com/sistemacompra/TesteMetodosCrud.java
@@ -1,2 +1,14 @@
-package br.com.sistemacompra;public class TesteMetodosCrud {
+package br.com.sistemacompra;
+
+import br.com.sistemacompra.model.dao.MercadoDAO;
+
+import java.sql.SQLException;
+
+public class TesteMetodosCrud {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        MercadoDAO mercadoDAO1 = new MercadoDAO();
+
+        mercadoDAO1.inserir("Bistek");
+    }
 }
